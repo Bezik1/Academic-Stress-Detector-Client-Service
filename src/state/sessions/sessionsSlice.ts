86e742f9315c04b62e56c77f899323f7ae8510f6
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-09-03 16:20:05 +0200

    Store Setup

---
+++ src/state/sessions/sessionsSlice.ts
@@ -0,0 +1,1 @@
+import { createAsyncThunk, createSlice, type PayloadAction } from "@reduxjs/toolkit"
---
+++ src/types/store/Session.ts
@@ -0,0 +1,19 @@
+export interface Session {
+    id: number;
+    stressLevel: number | null;
+    headache: number;
+    sleepQuality: number;
+    breathingProblems: number;
+    noiseLevel: number;
+    livingConditions: number;
+    safety: number;
+    basicNeeds: number;
+    academicPerformance: number;
+    studyLoad: number;
+    teacherStudentRelationship: number;
+    futureCareerConcerns: number;
+    socialSupport: number;
+    peerPressure: number;
+    extracurricularActivities: number;
+    bullying: number;
+}
