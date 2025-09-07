21c244898c4eac4b139007e6aecbc9b016275cab
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-09-07 14:55:04 +0200

    Adding urls

--- src/const/api.ts
+++ src/const/api.ts
@@ -1,8 +1,8 @@
-export const AUTH_URL = "http://localhost:8081/auth"
+export const AUTH_URL = "https://academic-stress-detector-auth-service.onrender.com/auth"
 export const LOGIN_URL = `${AUTH_URL}/login`
 export const REGISTER_URL = `${AUTH_URL}/register`
 
-export const USER_SERVICE_URL = "http://localhost:8080/api"
+export const USER_SERVICE_URL = "https://academic-stress-detector-auth-service.onrender.com/api"
 
 export const USERS_URL = `${USER_SERVICE_URL}/users`
 export const ME_URL = `${USERS_URL}/me`
