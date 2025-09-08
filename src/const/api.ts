d36bb5efeaa27f1bff19a5169dd3981aef9a10e6
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-09-08 16:25:17 +0200

    Fixing final typo

--- src/const/api.ts
+++ src/const/api.ts
@@ -10,5 +10,4 @@ export const ME_URL = `${USERS_URL}/me`
 export const SESSIONS_URL = `${USER_SERVICE_URL}/sessions`
 export const USER_SESSIONS_URL = `${SESSIONS_URL}/user`
 
-export const MODEL_SERVICE_URL = "https://academic-stress-detector-model-service.onrender.com/api/model"
-export const PREDICT_URL = `${MODEL_SERVICE_URL}/predict`
+export const PREDICT_URL = `${SESSIONS_URL}/predict`
